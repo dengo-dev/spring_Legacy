e3450b3cb42331cb0ec1958b6a9d8a491ead0b61
dengo23-dev <dengo23.dev@gmail.com>
2025-01-08 00:53:38 +0900

    HikariCP 설정

--- src/test/java/org/zerock/ex00/sample/DITests.java
+++ src/test/java/org/zerock/ex00/sample/DITests.java
@@ -6,12 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml") //spring test시 어떤 설정을 이용할 것인가?
 public class DITests {
   
   @Autowired //스프링을 띄운 후 아래 타입에 객체가 필요해(의존성 자동 주입)
   Restaurant restaurant;
+  
+  @Autowired
+  DataSource dataSource;
+  
+  @Test
+  public void testCon() throws Exception {
+    
+    Connection con = dataSource.getConnection();
+    System.out.println(con);
+    con.close();
+    
+  }
   
   @Test
   public void testExist() {
