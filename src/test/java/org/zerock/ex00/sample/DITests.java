37941df6fb1684f47e53faaacee9bb50d44c9b9e
dengo23-dev <dengo23.dev@gmail.com>
2025-01-08 00:24:08 +0900

    의존성 테스트

---
+++ src/test/java/org/zerock/ex00/sample/DITests.java
@@ -0,0 +1,20 @@
+package org.zerock.ex00.sample;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml") //spring test시 어떤 설정을 이용할 것인가?
+public class DITests {
+  
+  @Autowired //스프링을 띄운 후 아래 타입에 객체가 필요해(의존성 자동 주입)
+  Restaurant restaurant;
+  
+  @Test
+  public void testExist() {
+    System.out.println(restaurant);
+  }
+}
